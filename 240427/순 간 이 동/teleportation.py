08580737594646f3da0a4ac041b4604fc29949c8
UnrequiredOne <156090466+UnrequiredOne@users.noreply.github.com>
2024-04-27 05:05:55 +0900

    Title: 순 간 이 동, Time: 114ms, Memory: 24MB, Status: Passed - CodeTree

--- 240427/순 간 이 동/teleportation.py
+++ 240427/순 간 이 동/teleportation.py
@@ -3,6 +3,6 @@ A, B, x, y = map(int, input().split())
 A, B = (A, B) if A < B else (B, A)
 x, y =(x, y) if x < y else (y, x)
 
-length = abs(A-x) + abs(B-y)
+length = min(B - A, abs(A-x) + abs(B-y))
 
 print(length)
