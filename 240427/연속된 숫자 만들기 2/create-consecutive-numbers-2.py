8b2db223f86ff62f2dacfa581ff8f57ce0d5e45c
UnrequiredOne <156090466+UnrequiredOne@users.noreply.github.com>
2024-04-27 14:03:35 +0900

    Title: 연속된 숫자 만들기 2, Time: 231ms, Memory: 24MB, Status: Passed - CodeTree

--- 240427/연속된 숫자 만들기 2/create-consecutive-numbers-2.py
+++ 240427/연속된 숫자 만들기 2/create-consecutive-numbers-2.py
@@ -1,10 +1,13 @@
 arr = list(map(int, input().split()))
 
 answer = 0
-if arr[2] - arr[1] > 2 and arr[1] - arr[0]:
+
+if arr[2] - arr[1] > 2 and arr[1] - arr[0] > 2:
     answer = 2
 elif arr[2] - arr[1] <= 1 and arr[1] - arr[0] <= 1:
     answer = 0
+elif arr[2] - arr[1] <= 1 or arr[1] - arr[0] <= 1:
+    answer = 2
 else:
     answer = 1
 
