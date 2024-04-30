fd1b3014f511be9b767700d492f9c14a6148d89d
UnrequiredOne <156090466+UnrequiredOne@users.noreply.github.com>
2024-04-30 23:21:10 +0900

    Title: 원점으로부터의 거리, Time: 161ms, Memory: 26MB, Status: Wrong Answer - CodeTree

--- 240430/원점으로부터의 거리/distance-from-origin.py
+++ 240430/원점으로부터의 거리/distance-from-origin.py
@@ -8,7 +8,7 @@ for i in range(n):
 
 
 def compare(x, y):
-    return 1 if x[0] + x[1] > y[0] + y[1] or (x[0] + x[1] == y[0] + y[1] and x[2] > y[2]) else -1
+    return 1 if x[0] + x[1] > y[0] + y[1] or (x[0] + x[1] == y[0] + y[1] and x[2] < y[2]) else -1
 
 arr.sort(key=cmp_to_key(compare))
 
