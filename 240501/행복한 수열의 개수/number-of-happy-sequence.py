6d7575d1657f7e1e83f8cef21bce50c64e644beb
UnrequiredOne <156090466+UnrequiredOne@users.noreply.github.com>
2024-05-01 09:23:05 +0900

    Title: 행복한 수열의 개수, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240501/행복한 수열의 개수/number-of-happy-sequence.py
@@ -0,0 +1,38 @@
+n, m = map(int, input().split())
+
+arr = []
+answer = 0
+for i in range(n):
+    tmp = list(map(int, input().split()))
+    arr.append(tmp)
+
+for i in range(n):
+    prev_val = 0
+    cnt = 0
+    for j in range(n):
+        if prev_val == arr[i][j]:
+            cnt += 1
+        else:
+            if cnt >= m:
+                answer += 1
+            prev_val = arr[i][j]
+            cnt = 1
+    
+    if cnt >= m:
+        answer += 1
+
+for i in range(n):
+    prev_val = 0
+    cnt = 0
+    for j in range(n):
+        if prev_val == arr[j][i]:
+            cnt += 1
+        else:
+            if cnt >= m:
+                answer += 1
+            prev_val = arr[j][i]
+            cnt = 1
+    if cnt >= m:
+        answer += 1
+
+print(answer)
