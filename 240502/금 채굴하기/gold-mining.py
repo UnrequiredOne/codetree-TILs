823215a44a4a608fc8740d2d8c2aef8fd3769198
UnrequiredOne <156090466+UnrequiredOne@users.noreply.github.com>
2024-05-02 21:58:12 +0900

    Title: 금 채굴하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240502/금 채굴하기/gold-mining.py
@@ -0,0 +1,31 @@
+n, m = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(n)]
+
+values = []
+dx = [1, 0, -1, 0]
+dy = [0, 1, 0, -1]
+
+for i in range(n):
+    for j in range(n):
+        for k in range(n):
+            cost, value = 2 * k * (k + 1) + 1, 0
+            stack = [(i, j)]
+            visited = set()
+            visited.add((i, j))
+            while(stack):
+                x, y = stack.pop(0)
+                
+                if arr[x][y] == 1:
+                    value += 1
+
+                for a in range(4):
+                    nx, ny = x + dx[a], y + dy[a]
+                    if 0 <= nx < n and 0 <= ny < n and abs(nx - i) + abs(ny - j) <= k and (nx, ny) not in visited:
+                        stack.append((nx, ny))
+                        visited.add((nx, ny))
+
+            if cost < value * m:
+                values.append(value)
+                
+values.sort()
+print(values[-1])
