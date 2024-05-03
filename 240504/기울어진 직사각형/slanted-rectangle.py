0348281d25b2502408409f0ff9b237ba5ef76868
UnrequiredOne <156090466+UnrequiredOne@users.noreply.github.com>
2024-05-04 00:55:30 +0900

    Title: 기울어진 직사각형, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240504/기울어진 직사각형/slanted-rectangle.py
@@ -0,0 +1,32 @@
+n = int(input())
+arr = [list(map(int, input().split())) for _ in range(n)] 
+
+dx = [1, -1, -1, 1]
+dy = [-1, -1, 1, 1]
+m = 0
+for i in range(n):
+    for j in range(n):
+        k = 0
+        cnt = 0
+        nx, ny = i, j
+        tmp = arr[i][j]
+        
+        while True:
+            if 0 <= nx + dx[k] < n and 0 <= ny + dy[k] < n:
+                nx += dx[k]
+                ny += dy[k]
+                if nx == i and ny == j:
+                    break
+
+                tmp += arr[nx][ny]
+                cnt += 1            
+            
+            else:
+                if cnt == 0:
+                    break
+                cnt = 0
+                k = (k+1) % 4
+
+        m = max(m, tmp)
+
+print(m)
