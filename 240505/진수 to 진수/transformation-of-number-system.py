2ece5137f83e8c8fa957b674f6a82e6b08ff36e4
UnrequiredOne <156090466+UnrequiredOne@users.noreply.github.com>
2024-05-05 01:40:28 +0900

    Title: 진수 to 진수, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240505/진수 to 진수/transformation-of-number-system.py
+++ 240505/진수 to 진수/transformation-of-number-system.py
@@ -5,10 +5,10 @@ num = 0
 for i in range(len(n) - 1 , -1, -1):
     num += pow(a, i)
 
-answer = 0
+answer = []
 while num != 0:
     i = num % b
     num //= b
-    answer = answer * 10 + i
+    answer.append(str(i))
 
-print(answer)
+print(''.join(answer[::-1]))
