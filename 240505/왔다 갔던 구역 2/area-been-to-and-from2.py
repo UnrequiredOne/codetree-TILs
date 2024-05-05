8ef909f4d906c29f31c1fd3ea7adc7e36eee39c7
UnrequiredOne <156090466+UnrequiredOne@users.noreply.github.com>
2024-05-05 19:41:42 +0900

    Title: 왔다 갔던 구역 2, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240505/왔다 갔던 구역 2/area-been-to-and-from2.py
+++ 240505/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -1,18 +1,19 @@
 n = int(input())
 arr = [0 for _ in range(2001)]
-idx = 1001
+idx = 1000
 for i in range(n):
     x, cmd = input().split()
     x = int(x)
     if cmd == 'L':
-        for j in range(x):
+        for j in range(x+1):
             arr[idx] += 1
             idx -= 1
-
+        idx += 1
     elif cmd == 'R':
-        for j in range(x):
+        for j in range(x+1):
             arr[idx] += 1
             idx += 1
+        idx -= 1
 
 answer = 0
 
@@ -21,4 +22,24 @@ for i in range(len(arr)):
     if arr[i] >= 2:
         dots.append(i)
 
-print(len(dots))
+stack = []
+cnt = 0
+for item in dots:
+    if not stack:
+        stack.append(item)
+        cnt = 0
+    else:
+        prev_item = stack.pop()
+        if item - prev_item == 1:
+            stack.append(prev_item)
+            stack.append(item)
+            cnt += 1
+        else:
+            stack = []
+            stack.append(item)
+            answer += cnt
+            cnt = 0
+
+answer += cnt
+
+print(answer)
