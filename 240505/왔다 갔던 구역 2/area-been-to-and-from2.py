8790aa809abba7277e27ea781f28760fdc850bd2
UnrequiredOne <156090466+UnrequiredOne@users.noreply.github.com>
2024-05-05 20:10:43 +0900

    Title: 왔다 갔던 구역 2, Time: 141ms, Memory: 25MB, Status: Passed - CodeTree

--- 240505/왔다 갔던 구역 2/area-been-to-and-from2.py
+++ 240505/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -1,45 +1,32 @@
 n = int(input())
 arr = [0 for _ in range(2001)]
 idx = 1000
+lines = []
 for i in range(n):
     x, cmd = input().split()
     x = int(x)
+
     if cmd == 'L':
-        for j in range(x+1):
-            arr[idx] += 1
-            idx -= 1
-        idx += 1
+        x = x * -1
     elif cmd == 'R':
-        for j in range(x+1):
-            arr[idx] += 1
-            idx += 1
-        idx -= 1
+        x = x * 1
+
+    start = idx
+    idx += x
+    end = idx
+    if start > end:
+        end, start = start, end
+
+    lines.append((start, end))
 
 answer = 0
 
-dots = []
-for i in range(len(arr)):
+for line in lines:
+    for i in range(line[0], line[1]):
+        arr[i] += 1
+
+for i in range(2001):
     if arr[i] >= 2:
-        dots.append(i)
-
-stack = []
-cnt = 0
-for item in dots:
-    if not stack:
-        stack.append(item)
-        cnt = 0
-    else:
-        prev_item = stack.pop()
-        if item - prev_item == 1:
-            stack.append(prev_item)
-            stack.append(item)
-            cnt += 1
-        else:
-            stack = []
-            stack.append(item)
-            answer += cnt
-            cnt = 0
-
-answer += cnt
+        answer += 1
 
 print(answer)
