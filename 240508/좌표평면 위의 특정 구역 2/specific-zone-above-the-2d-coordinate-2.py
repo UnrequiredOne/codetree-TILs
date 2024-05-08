e7e6f243477751ca022d4915c49afc57486bf808
UnrequiredOne <156090466+UnrequiredOne@users.noreply.github.com>
2024-05-08 14:04:18 +0900

    Title: 좌표평면 위의 특정 구역 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240508/좌표평면 위의 특정 구역 2/specific-zone-above-the-2d-coordinate-2.py
@@ -0,0 +1,20 @@
+n = int(input())
+dots = []
+for i in range(n):
+    x, y = map(int, input().split())
+    dots.append((x, y))
+
+s = []
+for i in range(n):
+    a, b = dots.pop(0)
+    row = max(x for x, _ in dots) - min(x for x, _ in dots)
+    col = max(y for _, y in dots) - min(y for _, y in dots)
+    area = row * col
+    dots.append((a, b))
+    if area == 0:
+        continue
+    s.append(area)
+
+answer = min(s)
+
+print(answer)
