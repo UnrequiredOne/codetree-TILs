fa7637298b23640625dfae66d8d5faaec2dea23f
UnrequiredOne <156090466+UnrequiredOne@users.noreply.github.com>
2024-05-10 04:46:52 +0900

    Title: 높은 숫자의 카드가 이기는 게임, Time: 162ms, Memory: 26MB, Status: Passed - CodeTree

--- 240510/높은 숫자의 카드가 이기는 게임/a-high-number-of-cards-wins.py
+++ 240510/높은 숫자의 카드가 이기는 게임/a-high-number-of-cards-wins.py
@@ -8,10 +8,12 @@ for i in range(n):
 cnt = 0
 answer = 0
 for i in range(1, 2*n + 1):
-    if cnt != 0 and arr[i] == 0:
+    if arr[i] == 1:
+        cnt += 1
+    elif cnt != 0 and arr[i] == 0:
         cnt -= 1
         answer += 1
     else:
-        cnt += 1
+        continue
 
 print(answer)
