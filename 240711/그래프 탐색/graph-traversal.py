c1f8c8be1f4ee1cba8f0d3301818ee1dd4e2ef57
UnrequiredOne <156090466+UnrequiredOne@users.noreply.github.com>
2024-07-11 01:28:23 +0900

    Title: 그래프 탐색, Time: 100ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240711/그래프 탐색/graph-traversal.py
@@ -0,0 +1,23 @@
+n, m = map(int, input().split())
+
+arr = [[0 for _ in range(n + 1)] for _ in range(n + 1)]
+visited = [0 for _ in range(n + 1)]
+
+for _ in range(m):
+    x, y = map(int, input().split())
+    arr[x][y] = 1
+
+
+def dfs(v):
+    cnt = 1
+
+    for i in range(1, len(arr[v])):
+        if arr[v][i] == 1 and visited[i] == 0:
+            visited[i] = True
+            cnt += dfs(i)
+
+    return cnt
+
+
+visited[1] = 1
+print(dfs(1) - 1)
