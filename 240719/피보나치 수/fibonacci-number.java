9addaba6adae9e98cbb1872d5b4002e8d3623f3a
UnrequiredOne <156090466+UnrequiredOne@users.noreply.github.com>
2024-07-19 00:03:28 +0900

    Title: 피보나치 수, Time: 153ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240719/피보나치 수/fibonacci-number.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main{
+    private static int[] arr;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        arr = new int[N + 3];
+        Arrays.fill(arr, 0);
+        arr[0] = 1;
+        arr[1] = 1;
+        arr[2] = 1;
+        
+        System.out.println(fibo(N));
+    }
+
+    private static int fibo(int n) {
+        if (arr[n] != 0) return arr[n];
+
+        arr[n] = fibo(n-2) + fibo(n-1);
+        return arr[n];
+    }
+}
